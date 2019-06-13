621be44205d6a5a4609e454f56a7eaa6e2eb0a42
Charles maina <muragemaina09@gmail.com>
2019-06-13 10:32:01 +0300

    added todays and prev photos html and updated views

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,37 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome to my gallery')
+    return render(request, 'welcome.html')
 
 def photo_of_day(request):
     date = dt.date.today()
 
-    day = convert_dates(date)
-    html = f'''
-    <html>
-    <body>
-    <h1> Photos uploaded on {day} {date.day}-{date.month}-{date.year}</h1>
-    </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'all-photos/today-photos.html',{"date":date,})
 
 def previous_photos(request,past_date):
 
     try:
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
     except ValueError:
         raise Http404()
+        assert False
+
+        if date == dt.date.today():
+            return redirect(photo_of_day)
+
+        return render(request, 'all-photos/prev-photos.html',{"date":date,})
 
     day = convert_dates(date)
     html= f'''
-
-     day = convert_dates(date)
-    html = f'''
     <html>
     <body>
     <h1> Photos uploaded on {day} {date.day}-{date.month}-{date.year}</h1>
